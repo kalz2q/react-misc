790efa289f24cf3a2b3702359ba620035530794b
kalz2q <katsu95i@gmail.com>
2022-12-10 02:45:52 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => handleCopyClick(e)}>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          autoFocus
+          className="add-item-input"
+          placeholder="入力欄"
+        />
+        <input type="submit" value="Copy" className="submitButton" />
+      </form>
+      <form onSubmit={(e) => handlePasteButtonClick(e)}>
+        <input
+        />
+        <input type="submit" value="Paste" className="submitButton" />
+      </form>
     </div>
   );
 }
