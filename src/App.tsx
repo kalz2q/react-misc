99a128868dffa8f0e9fe0af397af68c376f1a422
kalz2q <katsu95i@gmail.com>
2022-12-12 12:01:25 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,17 @@
 import React from 'react';
 import './App.css';
-// [Tips] JavaScriptでCSVを読み込んでJSONを作る
-// JavaScriptでCSVデータ(項目行あり)をJSON形式に変換する
+// qiita
+// めっちゃ汎用的なアコーディオンメニューを作ろう
 
 function App() {
-
-  const jsonArray = csv2json(originalData);
-
-
   return (
     <div className="App">
-      <ul>
-        {jsonArray.map((v) => {
-          return (
-            <li
-              key={v["id"]}
-            >
-              <p>id: {v.id} </p>
-              <p>上の句: {v.上の句}  </p>
-              <p>下の句: {v.下の句} </p>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="js_acc_trigger acc_trigger">
+      </div>
+      <div className="js_acc_content acc_content"></div>
     </div >
   );
 }
 
-const originalData = `
-上の句, 下の句
-あさぼらけう "あらわれわたる"
-あきのたの, わがころもでは
-`
-
-const csv2json = (csvData: string) => {
-  let csvLines = csvData.split('\n').filter(Boolean); // 行ごとに分割する
-  const keyNames = csvLines[0].split(/[\n\s,;.]+/); // [ '上の句', ' 下の句' ]
-
-  type Item = {
-    "id": number;
-    "上の句": string;
-    "下の句": string
-  }
-
-  let jsonArray = [];
-
-  for (let i = 1; i < csvLines.length; i++) { // 0行目は見出しに使ったので1行目から
-    let a_line: Item = { id: 0, "上の句": "", "下の句": "" };
-    let csvArrayData = csvLines[i].split(/[\n\s\"\',;.]+/); // [ 'あさぼらけ', ' あらわれわたる' ]
-    a_line['id'] = i;
-    for (let j = 0; j < keyNames.length; j++) {
-      if (j === 0) a_line['上の句'] = csvArrayData[j]
-      else a_line['下の句'] = csvArrayData[j]
-    }
-    jsonArray.push(a_line);
-  }
-  return jsonArray;
-}
 
 export default App;
