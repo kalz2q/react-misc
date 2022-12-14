a1c7e03fe736b38e48493afc98139aeeb0106314
kalz2q <katsu95i@gmail.com>
2022-12-15 03:11:56 +0900

    M

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,21 @@
 import React from 'react';
 import './App.css';
-// 
+// 詳細cssアニメーション編
 
-const { useEffect, useRef, useState } = React;
+
+const { useState } = React;
 
 
 function App() {
-
-  // type Props = {
-  //   title: string;
-  //   withIcon?: boolean;
-  // };
-
-  const childElement = useRef<HTMLDivElement>(null);
-  const [showChildren, setshowChildren] = useState(false);
-  const [childHeight, setChildHeight] = useState(0);
   const [reverseIcon, setReverseIcon] = useState(false);
 
-  useEffect(() => {
-    if (childElement.current) {
-      const height = childElement.current?.clientHeight;
-      setChildHeight(height);
-    }
-  }, []);
-
   const handleClick = () => {
-    if (childElement.current) {
-      setshowChildren(!showChildren);
-    }
     setReverseIcon(!reverseIcon);
   };
 
-
   return (
-    <div className="App card">
-      <dl>
-        <div className="appear">
-          <div className="item" onClick={() => handleClick()}>親メニュー</div>
-          <div>子メニュー1</div>
-          <div>子メニュー1</div>
-        </div>
-      </dl>
+    <div className="App">
+
     </div >
   );
 }
